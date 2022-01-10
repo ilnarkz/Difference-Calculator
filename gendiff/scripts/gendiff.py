d3402c249fba141bf753e75df44c7c191bb2d227
ilnarkz <ilnar-16_04@mail.ru>
2022-01-10 22:42:11 +0300

    Create help output

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,13 @@
+import argparse
+
+
+def main():
+	parser = argparse.ArgumentParser(description='Generate diff')
+	parser.add_argument('first_file')
+	parser.add_argument('second_file')
+	parser.parse_args()
+	parser.print_help()
+
+
+if __name__ == '__main__':
+	main()
