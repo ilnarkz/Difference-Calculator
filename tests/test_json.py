f1299951c401e016c5d3cef0642c4c5cc34c0267
ilnarkz <ilnar-16_04@mail.ru>
2022-02-03 21:41:27 +0300

    change hexlet-check and add test

--- tests/test_json.py
+++ tests/test_json.py
@@ -2,4 +2,6 @@ from gen_diff.engine import generate_diff
 
 
 def test_json():
-	generate_diff()
+	file1 = open('./tests/fixtures/file1.json', 'r')
+	file2 = open('./tests/fixtures/file2.json', 'r')
+	assert generate_diff(file1, file2) == '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}'
