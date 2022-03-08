d2904bc0f2d2de4dcf0b85f5536623709ff41d17
ilnarkz <ilnar-16_04@mail.ru>
2022-03-08 18:10:54 +0300

    fix issues

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -12,24 +12,16 @@ def convert_plain(dictionary, parent=''):
             result.append(f"Property '{path.strip('.')}' was removed")
         elif type_ == 'added':
             if isinstance(value_, dict):
-                result.append(f"Property '{path.strip('.')}' was added with "
-                              f"value: [complex value]")
+                result.append(f"Property '{path.strip('.')}' was added with value: [complex value]")
             else:
-                result.append(f"Property '{path.strip('.')}' was added with "
-                              f"value: {lower(value_)}")
+                result.append(f"Property '{path.strip('.')}' was added with value: {lower(value_)}")
         elif type_ == 'changed':
             if isinstance(value_.get("old_value"), dict):
-                result.append(f"Property '{path.strip('.')}' was updated. "
-                              f"From [complex value] to "
-                              f"{lower(value_.get('new_value'))}")
+                result.append(f"Property '{path.strip('.')}' was updated. From [complex value] to {lower(value_.get('new_value'))}")
             elif isinstance(value_.get("new_value"), dict):
-                result.append(f"Property '{path.strip('.')}' was updated. "
-                              f"From {lower(value_.get('old_value'))} "
-                              f"to [complex value]")
+                result.append(f"Property '{path.strip('.')}' was updated. From {lower(value_.get('old_value'))} to [complex value]")
             else:
-                result.append(f"Property '{path.strip('.')}' was updated. "
-                              f"From {lower(value_.get('old_value'))} "
-                              f"to {lower(value_.get('new_value'))}")
+                result.append(f"Property '{path.strip('.')}' was updated. From {lower(value_.get('old_value'))} to {lower(value_.get('new_value'))}")
         elif type_ == 'nested':
             if isinstance(children_, dict):
                 result.append(convert_plain(children_, path))
--- gendiff/formatters/stylish.py
+++ gendiff/formatters/stylish.py
@@ -16,31 +16,16 @@ def convert_stylish(dictionary, depth=0):
         children_ = value_condition.get('children')
         indent = INDENT * (depth - 1)
         if type_ == 'removed':
-            result.append('{}{}{}: {}'.format(
-                indent, REMOVED, key, lower(value_, depth + 1)).rstrip()
-            )
+            result.append('{}{}{}: {}'.format(indent, REMOVED, key, lower(value_, depth + 1)))
         elif type_ == 'added':
-            result.append('{}{}{}: {}'.format(
-                indent, ADDED, key, lower(value_, depth + 1)).rstrip()
-            )
+            result.append('{}{}{}: {}'.format(indent, ADDED, key, lower(value_, depth + 1)))
         elif type_ == 'unchanged':
-            result.append('{}{}{}: {}'.format(
-                indent, UNCHANGED, key, lower(value_, depth + 1)).rstrip()
-            )
+            result.append('{}{}{}: {}'.format(indent, UNCHANGED, key, lower(value_, depth + 1)))
         elif type_ == 'changed':
-            result.append('{}{}{}: {}'.format(
-                indent, REMOVED, key,
-                lower(value_.get('old_value'), depth + 1)).rstrip()
-            )
-            result.append('{}{}{}: {}'.format(
-                indent, ADDED, key,
-                lower(value_.get('new_value'), depth + 1)).rstrip()
-            )
+            result.append('{}{}{}: {}'.format(indent, REMOVED, key, lower(value_.get('old_value'), depth + 1)))
+            result.append('{}{}{}: {}'.format(indent, ADDED, key, lower(value_.get('new_value'), depth + 1)))
         else:
-            result.append('{}{}{}: {}'.format(
-                indent, UNCHANGED, key,
-                convert_stylish(children_, depth)).rstrip()
-            )
+            result.append('{}{}{}: {}'.format(indent, UNCHANGED, key, convert_stylish(children_, depth)))
     output = itertools.chain('{', result, [indent + '}'])
     return '\n'.join(output)
 
@@ -53,9 +38,7 @@ def lower(item, depth):
         indent = INDENT * depth
         end_indent = INDENT * (depth - 1)
         for key, key_value in item.items():
-            result.append('{}{}: {}'.format(
-                indent, key, lower(key_value, depth + 1))
-            )
+            result.append('{}{}: {}'.format(indent, key, lower(key_value, depth + 1)))
         output = itertools.chain('{', result, [end_indent + '}'])
         return '\n'.join(output)
     return json.dumps(item)
