1cef5f8bb71548d6953344e357ac8ea67dadfa3b
ilnarkz <ilnar-16_04@mail.ru>
2022-03-10 23:16:40 +0300

    fix issues after first checking

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,9 +1,7 @@
 from gendiff.engine import get_external_view
-from gendiff.formatters.json import format_to_json
-from gendiff.formatters.plain import format_to_plain
+from gendiff.formatter import change_format
 from gendiff.parsing import parse
 from gendiff.read_file import get_full_path
-from gendiff.formatters.stylish import format_to_stylish
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
@@ -12,8 +10,4 @@ def generate_diff(file_path1, file_path2, format_name='stylish'):
     file1 = parse(path1)
     file2 = parse(path2)
     common_dict = get_external_view(file1, file2)
-    if format_name == 'plain':
-        return format_to_plain(common_dict)
-    if format_name == 'json':
-        return format_to_json(common_dict)
-    return format_to_stylish(common_dict)
+    return change_format(common_dict, format_name)
