9be7fd8872c22ee5621a270e61ce636615358f31
ilnarkz <ilnar-16_04@mail.ru>
2022-04-21 20:38:46 +0300

    fix final issues

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,9 +5,9 @@ from gendiff.read_file import read_file
 
 
 def generate_diff(file_path1, file_path2, format_name='stylish'):
-    data_of_file1, file_extension1 = read_file(file_path1)
-    data_of_file2, file_extension2 = read_file(file_path2)
-    dictionary1 = parse(data_of_file1, file_extension1)
-    dictionary2 = parse(data_of_file2, file_extension2)
+    data_of_string1, file_format1 = read_file(file_path1)
+    data_of_string2, file_format2 = read_file(file_path2)
+    dictionary1 = parse(data_of_string1, file_format1)
+    dictionary2 = parse(data_of_string2, file_format2)
     common_dict = make_diff(dictionary1, dictionary2)
     return render(common_dict, format_name)
